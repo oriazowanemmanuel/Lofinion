27f3a29a2990935cf27486e3cced816df0b409e3
mac <mac@macs-MacBook-Pro.local>
2024-04-03 11:29:25 +0100

    Moved video feature to the header in app.js

--- src/App.js
+++ src/App.js
@@ -36,6 +36,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Reading Companion - Lofi Music & Calm Videos</h1>
+      <iframe 
+      width="1120" 
+      height="630" 
+      src="https://www.youtube.com/embed/4ROrW727q_s?si=GUuooLbWU-dFde_l" 
+      title="YouTube video player" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      referrerpolicy="strict-origin-when-cross-origin" 
+      allowfullscreen>
+        
+      </iframe>
+      
       </header>
 
     <main>
